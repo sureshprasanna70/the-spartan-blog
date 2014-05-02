fd00e411687207e80bb11903fbfd6b4092eaf735
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-05-03 01:46:43 +0530

    For reasons

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#Experimental Models 
+#Will be replaced
 class Publisher(models.Model):
 name = models.CharField(max_length=30)
 address = models.CharField(max_length=50)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-	html = "<html><body>It is now</body></html>"
+	html = "<html><body>djfks</body></html>"
 	return HttpResponse(html)
 
