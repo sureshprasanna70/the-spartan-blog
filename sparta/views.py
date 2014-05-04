441adfb8b7701dbfa3624deb847068d91195105a
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-05-05 00:56:16 +0530

    Passing list to a Django template

--- sparta/views.py
+++ sparta/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.utils import simplejson
 from blog.models import Blog_Post
 import datetime
 def index(request):
 	Title=Blog_Post.objects.all()[0].title
 	Contents=Blog_Post.objects.all()[0].contents
-	#print (publisher)
-	contents=["name"]
-	#for i in range(0,publisher):
-	#		contents[i]=Blog_Post.objects.get(id=i+1)
+	publisher=Blog_Post.objects.all().count()
+	print (publisher)
+	All_title=[]
+	All_post=[]
+	for i in range(0,publisher):
+			All_title.append(Blog_Post.objects.all()[i].title)
+			All_post.append(Blog_Post.objects.all()[i].contents)
 	#return HttpResponse(contents)
-	return render_to_response('index.html', {'title':Title,'content':Contents})
+	
+	return render_to_response('index.html', {'title':All_title,'content':All_post,'totalCount':i})
 
 
 
