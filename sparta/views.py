17a3b4099d2a0c0eb0f7c91e8faf9ab537bb69ea
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-05-06 16:02:15 +0530

    Added custom.css & jquery.js

--- sparta/views.py
+++ sparta/views.py
@@ -7,14 +7,14 @@ def index(request):
 	Contents=Blog_Post.objects.all()[0].contents
 	publisher=Blog_Post.objects.all().count()
 	print (publisher)
-	All_title=[]
+	
 	All_post=[]
 	for i in range(0,publisher):
-			All_title.append([Blog_Post.objects.all()[i].title,Blog_Post.objects.all()[i].contents])
-			All_post.append(Blog_Post.objects.all()[i].contents)
+			All_post.append([Blog_Post.objects.all()[i].title,Blog_Post.objects.all()[i].contents])
+			
 	#return HttpResponse(contents)
 	
-	return render_to_response('index.html', {'title':All_title,'content':All_post,'totalCount':i})
+	return render_to_response('index.html', {'title':"Spartanz Blog",'content':All_post,'totalCount':i})
 
 
 
